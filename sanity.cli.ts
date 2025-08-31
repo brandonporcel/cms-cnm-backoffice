1a4cf9880a7fc98cee456a08c885d0e5457a2900
brandonporcel <brandon7.7porcel@gmail.com>
2025-08-31 17:45:09 -0300

    feat: add in index config film schema

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -3,11 +3,12 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: '4ov954nv',
-    dataset: 'production'
+    dataset: 'production',
   },
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
    */
   autoUpdates: true,
+  studioHost: 'cms-cmn',
 })
--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -6,12 +6,14 @@ import person from './person'
 import screening from './screening'
 import plotSummary from './plotSummary'
 import plotSummaries from './plotSummaries'
+import film from './film'
 
 export const schemaTypes = [
   // Document types
   movie,
   person,
   screening,
+  film,
 
   // Other types
   blockContent,
